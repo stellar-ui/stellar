45fc7586ac4555a95a3087795abdc91d9075ef58
Nima Dehnashi <nima.dehnashi@gmail.com>
2021-02-21 00:52:11 -0800

    chore(storybook): upgrade storybook to latest

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,4 @@
 module.exports = {
   addons: ['@storybook/addon-docs'],
   stories: ['../src/**/*.stories.tsx'],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-        },
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
-        },
-      ],
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-  },
 };
